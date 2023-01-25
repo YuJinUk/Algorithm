5d39253010d46057569dcc58b2a881703c6535db
YuJinUk <yujinuk5140@gmail.com>
2023-01-25 23:56:25 +0900

    택배상자

---
+++ programmers/Lv2/택배상자.py
@@ -0,0 +1,20 @@
+from collections import deque
+def solution(order):
+    order = deque(order)
+    subblet = deque()
+    truck = 0
+
+    for box in range(1, len(order) +1):
+        if order[0] != box:
+            subblet.append(box)
+            continue
+        order.popleft()
+        truck += 1
+        while len(subblet) != 0 and len(order) > 0:
+            if order[0] == subblet[-1]:
+                subblet.pop()
+                truck += 1
+                order.popleft()
+            else:
+                break
+    return truck
