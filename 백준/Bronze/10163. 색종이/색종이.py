fd70fcecdfbce9a9b519101bee8d33c5697babf1
YuJinUk <122508530+YuJinUk@users.noreply.github.com>
2023-02-05 18:17:55 +0900

    Update 색종이.py

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,12 +1,14 @@
 paper = int(input())
 n = [list(map(int, input().split())) for _ in range(paper)]
-
-board = [[0]* 1001 for _ in range(1001)]
+a = []
+for i in n:
+    a.append(max(i[0] + i[2], i[1]+i[3]))
+b = max(a)
+board = [[0]* (b+1) for _ in range(b+1)] # 가장 큰 맵을 기준으로 board를 생성
 
 for idx, k in enumerate(n):
-    for i in range(k[0], k[0] + k[2]):
-        for j in range(k[1], k[1] + k[3]):
-            board[i][j] = idx+1
+    for j in range(k[1], k[1] + k[3]):
+        board[j][k[0]:k[0]+k[2]] = [idx+1] * k[2]
 
 board_new = []
 for i in board:
