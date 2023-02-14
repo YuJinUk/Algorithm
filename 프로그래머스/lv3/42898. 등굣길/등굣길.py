ad8af88685762f4106ea15c4665e39ef72bc1731
YuJinUk <122508530+YuJinUk@users.noreply.github.com>
2023-02-14 11:51:57 +0900

    Update 등굣길.py

--- 프로그래머스/lv3/42898. 등굣길/등굣길.py
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.py
@@ -12,3 +12,27 @@ def solution(m, n, puddles):
             if 0<=j-1<=m and [j,i] not in puddles:
                 board[i][j] += board[i][j-1]
     return board[-1][-1] % (10**9 +7)
+    # 시간초과 산물
+    # dp = [[] for _ in range(n + m - 1)] # 지나치는 순서에 따른 모든 리스트를 생성한 dp 
+    # dp[0].append(start)
+    # for i in range(1, len(dp)):
+    #     for x, y in dp[i-1]:
+    #         if x+1<=m and [x+1, y] not in puddles:
+    #             dp[i].append((x+1,y))
+    #         if y+1<=n and [x, y+1] not in puddles:
+    #             dp[i].append((x,y+1))
+    # return len(dp[-1]) 
+    
+    # dfs로 탐색하면 
+    # result = 0
+    # print(dp)
+    # while dp:
+    #     x, y = dp.popleft()
+    #     if x == m and y == n:
+    #         result += 1
+    #         continue
+    #     if x+1<=m and [x+1, y] not in puddles:
+    #         dp.append((x+1, y))
+    #     if y+1<=n and [x, y+1] not in puddles:
+    #         dp.append((x, y+1))
+    # return result%(10**9 +7)
