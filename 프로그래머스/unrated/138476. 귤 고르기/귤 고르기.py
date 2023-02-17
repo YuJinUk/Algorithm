319360010a2e3217624602efce5a6a38dd2fa31c
YuJinUk <122508530+YuJinUk@users.noreply.github.com>
2023-02-18 00:06:38 +0900

    [unrated] Title: 귤 고르기, Time: 146.79 ms, Memory: 18 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
@@ -1,4 +1,3 @@
-# 정답
 from collections import Counter
 def solution(k, t): # k: 총 뽑을 귤의 수, t : 귤 사이즈 리스트 
    cnt_t = Counter(t)
@@ -18,18 +17,17 @@ def solution(k, t): # k: 총 뽑을 귤의 수, t : 귤 사이즈 리스트
 # 재귀로 새로 풀어보기
 # from collections import Counter
 # cnt = 0 # 귤의 종류 수
-# def re(k,nums):
-#     global cnt
-#     max_nums = nums.pop(0)
-#     if max_nums >= k: # k보다 max(num)이 크다면 함수가 끝남
-#         cnt +=1
+# def re(a,nums):
+#     cnt = 1
+#     max_nums = nums[-1]
+#     if max_nums >= a: # k보다 max(num)이 크다면 함수가 끝남
 #         return cnt
 #     else :
 #         # nums에서 가장 큰 수를 제거함
-#         cnt +=1
-#         return re(k-max_nums, nums) # 재귀
+#         nums.pop()
+#         cnt += re(a-max_nums, nums) # 재귀
+#     return cnt
 # def solution(k, t): # k: 총 뽑을 귤의 수, t : 귤 사이즈 리스트 
 #     cnt_t = Counter(t)
-#     num = list(cnt_t.values())
-#     num = sorted(num, reverse = True)
+#     num = sorted(list(cnt_t.values()))
 #     return re(k,num)
