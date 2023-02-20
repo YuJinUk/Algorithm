9c8d5539dec129403aca5a68e609d1abeb9c9137
YuJinUk <122508530+YuJinUk@users.noreply.github.com>
2023-02-20 22:14:02 +0900

    [level 3] Title: 섬 연결하기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.py
@@ -2,22 +2,23 @@
 def solution(n, costs):
     parent = [i for i in range(n+1)]
     costs.sort(key = lambda x: x[2]) # 사이즈가 작은 애들부터 연결시키기 위해
+    
     def find(x):
         if parent[x] != x:
-            parent[x] = find(parent[x])
+            parent[x] = find(parent[x]) # 가장 높은 parent를 찾기 위해 재귀를 돌림
         return parent[x]
     
     def union(x,y):
-        x = find(x)
-        y = find(y)
-        if x > y:
+        x = find(x) # 가장 높은 parent를 찾는 과정
+        y = find(y) # 가장 높은 parent를 찾는 과정
+        if x > y: # 만약 x가 y보다 크다면 y가 parent, x가 child
             parent[x] = y
         elif x < y:
             parent[y] = x
     
     ans = 0
     for x1, x2, value in costs:
-        if find(x1) != find(x2):
+        if find(x1) != find(x2): # x1과 x2가 같다면 자기자신으로 돌아가는 것이므로 value가 증가하기때문에 제외하기 위함
             union(x1,x2)
             ans += value
     return ans
