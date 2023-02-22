8a4dd698ed0617b5525e8eb4d1ee5e2ec124edf4
YuJinUk <122508530+YuJinUk@users.noreply.github.com>
2023-02-22 23:18:14 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/43164. 여행경로/여행경로.py
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.py
@@ -1,33 +1,17 @@
 def solution(tickets):
-    routes = dict()
-    for t in tickets:
-        routes[t[0]] = routes.get(t[0], []) + [t[1]]
-    for r in routes:
-        routes[r].sort(reverse=True)
+    dic = dict()
+    for ticket in tickets:
+        dic.setdefault(ticket[0],[])
+        dic[ticket[0]].append(ticket[1])
+    for r in dic:
+        dic[r].sort(reverse=True)
     stack = ["ICN"]
     path = []
     while len(stack) > 0:
         top = stack[-1]
-        if top not in routes or len(routes[top]) == 0:
+        if top not in dic or len(dic[top]) == 0:
             path.append(stack.pop())
         else:
-            stack.append(routes[top][-1])
-            routes[top] = routes[top][:-1]
+            stack.append(dic[top][-1])
+            dic[top] = dic[top][:-1]
     return path[::-1]
-# from collections import deque
-# def solution(tickets):
-#     dic = dict()
-#     for ticket in tickets:
-#         dic.setdefault(ticket[0],[])
-#         dic[ticket[0]].append(ticket[1])
-#     start_p = 'ICN'
-#     ans = deque()
-#     visited = deque()
-#     visited.append(start_p)
-#     while visited:
-#         next_p = visited.pop()
-#         for i in next_p:
-#             if i not in visited:
-#                 visited.append(i)
-#         break
-#     return dic
