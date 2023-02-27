edfb1355d300851a8adda1e5a99f55f35b127578
YuJinUk <122508530+YuJinUk@users.noreply.github.com>
2023-02-27 23:23:02 +0900

    [Gold V] Title: 호석이 두 마리 치킨, Time: 184 ms, Memory: 117804 KB -BaekjoonHub

--- 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.py
+++ 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.py
@@ -4,11 +4,14 @@ n, m = map(int, input().split())
 dic = dict()
 for i in range(1,n+1):
     dic.setdefault(i, [])
+
+answer = deque([[0]*(n+1) for _ in range(n+1)])
+
 for _ in range(m):
     parent, child = map(int, input().split())
     dic[parent].append(child)
     dic[child].append(parent)
-answer = deque([[0]*(n+1) for _ in range(n+1)])
+
 for start in dic:
     visited = [False for _ in range(n+1)]
     dist = 0
@@ -18,18 +21,17 @@ for start in dic:
         v, dist = result.popleft()
         if not visited[v]:
             visited[v] = True
-            dist += 1
             for i in dic[v]:
                 if not visited[i]:
-                    result.append((i, dist))
-                    answer[start][i] = dist
+                    result.append((i, dist+1))
+                    answer[start][i] = dist+1
+                    
 combi = list(combinations(list(range(1, n+1)), 2))
 checking_list = []
+
 for c1, c2 in combi:
-    a = answer[c1]
-    b = answer[c2]
     cnt = 0
-    for check1, check2 in zip(a, b):
+    for check1, check2 in zip(answer[c1], answer[c2]):
         cnt += check2 if check1 > check2 else check1
     checking_list.append([c1,c2,cnt*2])
 checking_list.sort(key = lambda x: (x[2], x[0], x[1]))
