935945d21f9ce164fb0865f76afa2da1844d8647
YuJinUk <122508530+YuJinUk@users.noreply.github.com>
2023-02-28 21:08:40 +0900

    [D3] Title: 농작물 수확하기, Time: 238 ms, Memory: 63,032 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
@@ -1,17 +1,9 @@
-# 2805 농작물 수확하기
 T = int(input())
 for tt in range(1, T+1):
     n = int(input())
-    matrix = [list(map(int, list(input()))) for _ in range(n)]
-    result = []
-    rm = n//2
-    for idx, i in enumerate(matrix):
-        m = abs(rm - idx)
-        for k in range(m):
-            i.pop()
-            i.pop(0)
-        result.append(i)
-    ans = 0
-    for i in result:
-        ans += sum(i)
-    print(f'#{tt} {ans}')
+    answer = 0
+    column = n//2
+    for i in range(n):
+        board = list(map(int, list(input())))
+        answer += sum(board[column-i:column+i+1]) if i <= column else sum(board[column -(n-1-i) :column + n-i])
+    print(f'#{tt} {answer}')
