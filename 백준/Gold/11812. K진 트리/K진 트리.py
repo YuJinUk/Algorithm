788b6305e41f82290687fc792c33e8ad01c4bae1
YuJinUk <122508530+YuJinUk@users.noreply.github.com>
2023-03-09 17:16:08 +0900

    [Gold III] Title: K진 트리, Time: 316 ms, Memory: 127956 KB -BaekjoonHub

---
+++ 백준/Gold/11812. K진 트리/K진 트리.py
@@ -0,0 +1,18 @@
+n, k, m = map(int, input().split())
+node = [list(map(int, input().split())) for _ in range(m)]
+
+for i, j in node:
+    a, b = min(i,j), max(i,j)
+    if k == 1:
+        print(b-a)
+        continue
+    cnt = 0
+    while True:
+        b = (b-2)//k + 1
+        if a == b:
+            f = 1
+            break
+        elif a > b:
+            a, b = b, a
+        cnt += 1
+    print(cnt+1)
