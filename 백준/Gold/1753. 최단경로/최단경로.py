52f174db84c8690f3e14d15a4962681b68d5a88e
YuJinUk <122508530+YuJinUk@users.noreply.github.com>
2023-03-12 22:36:11 +0900

    [Gold IV] Title: 최단경로, Time: 944 ms, Memory: 145920 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -0,0 +1,32 @@
+import heapq
+V, E = map(int, input().split())
+k = int(input())
+node = {i:[] for i in range(1, V+1)}
+for i in range(1, E+1):
+    u, v, w = map(int, input().split())
+    node[u].append((v,w))
+
+def dijkstra(graph, start):
+    dic_distance[start] = 0
+    queue = []
+    heapq.heappush(queue, [0, start])
+    while queue:
+        now_dist, now_lo = heapq.heappop(queue)
+
+        if dic_distance[now_lo] < now_dist:
+            continue
+
+        for new_lo, new_dist in graph[now_lo]:
+            dist = now_dist + new_dist
+            if dist < dic_distance[new_lo]:
+                dic_distance[new_lo] = dist
+                heapq.heappush(queue, [dist, new_lo])
+    return dic_distance
+
+dic_distance = [float('inf')] * (V+1)
+result = dijkstra(node, k)
+for i in range(1, V+1):
+    if result[i] == float('inf'):
+        print('INF')
+    else:
+        print(result[i])
