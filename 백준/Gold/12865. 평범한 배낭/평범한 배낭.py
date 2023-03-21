63617d8df2eb441fad3140ccfcf87baa36e4c7b7
YuJinUk <122508530+YuJinUk@users.noreply.github.com>
2023-03-21 20:24:46 +0900

    [Gold V] Title: 평범한 배낭, Time: 208 ms, Memory: 115272 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,16 +1,10 @@
 from sys import stdin
 n, k = map(int, stdin.readline().split())
+items = sorted([tuple(map(int, stdin.readline().split())) for _ in range(n)])
 
-wv = [[0,0]] + [list(map(int, stdin.readline().rstrip().split())) for _ in range(n)]
-dp = [[0]*(k+1) for _ in range(n+1)]
-for i in range(1, n+1):
-    for j in range(1, k+1):
-        w = wv[i][0]
-        v = wv[i][1]
+values, weights= [0]*(k+1), set()
 
-        if j < w:
-            dp[i][j] = dp[i-1][j]
-        else:
-            dp[i][j] = max(dp[i-1][j], dp[i-1][j-w]+v)
-# print(dp)
-print(dp[n][k])
+for i in items:
+    for j in range(k,i[0]-1,-1):
+        values[j] = max(values[j], values[j-i[0]]+i[1])
+print(max(values))
