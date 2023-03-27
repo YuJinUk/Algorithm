814f4f85459a22d1b8164831e54790ad76a1c5ed
YuJinUk <122508530+YuJinUk@users.noreply.github.com>
2023-03-27 11:31:35 +0900

    [Gold III] Title: 박스 채우기, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Gold/1493. 박스 채우기/박스 채우기.py
+++ 백준/Gold/1493. 박스 채우기/박스 채우기.py
@@ -4,21 +4,18 @@ n = int(stdin.readline())
 cube = [tuple(map(int, stdin.readline().split())) for _ in range(n)]
 cube.sort(reverse=True)
 
-v = l*w*h
-cnt = 0
-check = 0
-answer = 0
+v = l*w*h # 총 넓이
+cnt = 0 # 정육각형 사이즈 별 부피에 따라 제거해줘야하는 상자의 개수
+check = 0 # 최종 넓이
+answer = 0 # 정육각형 개수
 for s, c in cube:
     s = 1 << s
-    # print('sss', s)
     cnt *= 8
     pos = (l//s) * (w//s) * (h//s) - cnt
-    min_value = min(pos, c)
+    min_value = pos if pos < c else c
     check += min_value * (s**3)
     cnt += min_value
     answer += min_value
-    # print(pos, cnt)
-    # print('check',check)
 if check == v:
     print(answer)
 else:
