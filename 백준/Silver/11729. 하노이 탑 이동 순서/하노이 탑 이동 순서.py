23f04f74f3788a8d2eb17c8c4bcf76a52715a522
YuJinUk <122508530+YuJinUk@users.noreply.github.com>
2023-05-13 13:56:28 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 2344 ms, Memory: 231644 KB -BaekjoonHub

---
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -0,0 +1,30 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+def solution(n):
+    dp = deque([[[1,3]],[[1,2],[1,3],[2,3]]])
+    if n<3:
+        return dp[n-1]
+    for i in range(2, n):
+        prior = dp[i-1]
+        first = []
+        second = []
+        for a,b in prior:
+            if a == 2 : a = 3
+            elif a == 3: a = 2
+            if b == 2 : b = 3
+            elif b == 3 : b = 2
+            first.append([a,b])
+        for a,b in prior:
+            if a == 2 : a = 1
+            elif a == 1 : a = 2
+            if b == 2 : b = 1
+            elif b == 1 : b = 2
+            second.append([a,b])
+        dp.append(first+[[1,3]]+second)
+    return dp[-1]
+answer = solution(int(input()))
+print(len(answer))
+for i in answer:
+    print(*i)
