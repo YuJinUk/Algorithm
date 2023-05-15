211b35be17e1015096329b6b16dc542bf9ebd787
YuJinUk <122508530+YuJinUk@users.noreply.github.com>
2023-05-15 12:56:09 +0900

    [level 2] Title: 전화번호 목록, Time: 94.60 ms, Memory: 28.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -2,7 +2,7 @@ def solution(p):
 
     p.sort()
     for i in range(len(p)-1): 
-        if p[i] == (p[i+1])[:len(p[i])] : 
+        if p[i] == p[i+1][:len(p[i])] : 
             return False
 
     return True
