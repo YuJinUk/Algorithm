c9d1f0f2f008108688337939f1253a2cf64c384a
YuJinUk <122508530+YuJinUk@users.noreply.github.com>
2023-09-14 09:20:59 +0900

    [Gold V] Title: 동전 2, Time: 400 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -5,13 +5,12 @@ N, K = map(int, input().split())
 coin = [int(input()) for _ in range(N)]
 coin.sort(reverse=True)
 
-dp = [10001] * (K+1)
-dp[0] = 0
+dp = [0] + [10001] * K
 
 for num in coin:
     for i in range(num, K+1):
         dp[i] = min(dp[i],dp[i-num]+1)
-if dp[K] == 10001:
+if dp[-1] == 10001:
     print(-1)
 else:
     print(dp[-1])
