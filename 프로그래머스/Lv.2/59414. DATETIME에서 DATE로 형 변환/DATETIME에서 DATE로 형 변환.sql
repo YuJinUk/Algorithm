aa96dd16547d6b4f4b9157b5c98d4f8e9562bffe
YuJinUk <122508530+YuJinUk@users.noreply.github.com>
2024-01-12 16:48:48 +0900

    [level Lv.2] Title: DATETIME에서 DATE로 형 변환, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/59414. DATETIME에서 DATE로 형 변환/DATETIME에서 DATE로 형 변환.sql
@@ -0,0 +1,18 @@
+-- 코드를 입력하세요
+SELECT ANIMAL_ID, NAME, DATE_FORMAT(DATETIME, '%Y-%m-%d') AS '날짜' FROM ANIMAL_INS
+ORDER BY ANIMAL_ID
+
+
+
+
+
+
+
+
+
+
+
+
+# SELECT ANIMAL_ID, NAME, date_format(DATETIME, '%Y-%m-%d') as '날짜'
+# from animal_ins
+# order by animal_id
