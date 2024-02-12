73b97a86292421da458a48b110f6e2e42e42c82b
YuJinUk <122508530+YuJinUk@users.noreply.github.com>
2024-02-12 11:25:28 +0900

    [level 2] Title: 2 x n 타일링, Time: 27.07 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.py
@@ -0,0 +1,10 @@
+def solution(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    a, b = 1, 2
+    for i in range(3,n+1):
+        a, b = b, a+b
+    return b%1000000007
+    
