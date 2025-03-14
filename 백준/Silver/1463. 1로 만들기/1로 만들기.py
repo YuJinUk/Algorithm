e7655d37a2a2892aeacfdfecf6059b2f0383962c
YuJinUk <122508530+YuJinUk@users.noreply.github.com>
2025-03-15 02:43:17 +0900

    [Silver III] Title: 1로 만들기, Time: 436 ms, Memory: 40224 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,9 +1,15 @@
-n = int(input())
-dp = [0] * (n+1)
-for i in range(2, n+1):
+import sys
+
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+
+dp = [0] * (n + 1)
+for i in range(2, n + 1):
     dp[i] = dp[i-1] + 1
     if not i % 2:
         dp[i] = min(dp[i], dp[i//2] + 1)
     if not i % 3:
         dp[i] = min(dp[i], dp[i//3] + 1)
+
 print(dp[-1])
