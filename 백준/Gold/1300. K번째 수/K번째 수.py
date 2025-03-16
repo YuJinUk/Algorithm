71637da290c6bb08790633ad7dbfa3c29156ab02
YuJinUk <122508530+YuJinUk@users.noreply.github.com>
2025-03-17 05:13:38 +0900

    [Gold I] Title: K번째 수, Time: 840 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.py
+++ 백준/Gold/1300. K번째 수/K번째 수.py
@@ -1,13 +1,12 @@
-# 프로그래머스 처리가능한 손님 수
-n = int(input())
-k = int(input())
-start, end = 1, n**2
+N = int(input())
+K = int(input())
+start, end = 1, N**2
 while start <= end:
     mid = (start + end) // 2
     check = 0
-    for i in range(1, n+1):
-        check += min(mid//i, n)
-    if check >= k:
+    for i in range(1, N + 1):
+        check += min(mid // i, N)
+    if check >= K:
         end = mid - 1
     else:
         start = mid + 1
