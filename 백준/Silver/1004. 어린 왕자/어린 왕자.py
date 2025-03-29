b2a05578e117f0c18d2af82b600ce34605fca5b7
YuJinUk <122508530+YuJinUk@users.noreply.github.com>
2025-03-29 15:40:05 +0900

    [Silver III] Title: 어린 왕자, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1004. 어린 왕자/어린 왕자.py
+++ 백준/Silver/1004. 어린 왕자/어린 왕자.py
@@ -1,5 +1,7 @@
 import sys
 
+input = sys.stdin.readline
+
 TC = int(input())
 
 answer = []
@@ -14,21 +16,15 @@ for _ in range(TC):
     start_x, start_y, end_x, end_y = map(int, input().split())
     
     N = int(input())
-
-    planet = []
+    result = 0
     for _ in range(N):
         cx, cy, cr = map(int, input().split())
-        planet.append((cx, cy, cr))
+        start_result, end_result = calculate_distance(start_x, start_y, cx, cy, cr), calculate_distance(end_x, end_y, cx, cy, cr)
+        if start_result == end_result:
+            continue
+        result += 1
     
-    start_point_cnt, end_point_cnt = 0, 0
-    
-    for cx, cy, cr in planet:
-        if calculate_distance(start_x, start_y, cx, cy, cr) and not calculate_distance(end_x, end_y, cx, cy, cr):
-            start_point_cnt += 1
-        elif not calculate_distance(start_x, start_y, cx, cy, cr) and calculate_distance(end_x, end_y, cx, cy, cr):
-            end_point_cnt += 1
-    
-    answer.append(start_point_cnt + end_point_cnt)
+    answer.append(result)
 
 for ans in answer:
     print(ans)
